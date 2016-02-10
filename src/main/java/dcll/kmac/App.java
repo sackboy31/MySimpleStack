ef34754925bf6219cb2dc9e7c0ba9acee873a861
sackboy31 <sackboy31@live.fr>
2016-02-10 17:07:02 +0100

    Ex2q3

---
+++ src/main/java/dcll/kmac/App.java
@@ -0,0 +1,13 @@
+package dcll.kmac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
